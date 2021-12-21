136fefb4eed28ef4a9e80926f4e6abd126e2fce7
Chandler <chandler_morgan@hotmail.com>
2021-12-21 03:11:22 -0800

    Basic rounds working correctly

--- RPSgame.js
+++ RPSgame.js
@@ -1,17 +1,29 @@
 // Just Defining variables to be changed later
-let computerChoice = 'Computer is failing';
-let playerChoice = 'Player is not working';
+let computerChoice = 'Computer choice is broken';
+let playerChoice = 'Player choice is broken';
+let playerScore = 0;
+let computerScore = 0;
+
+function playerWin() {
+    playerScore = playerScore + 1;
+    console.log('You won the round!')
+}
+
+function computerWin() {
+    computerScore = computerScore + 1;
+    console.log('You lost the round!')
+}
 
 function computerSelection() {
     computerChoice = Math.floor(Math.random() * 3);
         if (computerChoice === 0) {
-            computerChoice = 'Rock';
+            computerChoice = 'rock';
         }
         else if (computerChoice === 1) {
-            computerChoice = 'Paper';
+            computerChoice = 'paper';
         }
         else {
-            computerChoice = 'Scissors';
+            computerChoice = 'scissors';
         }
 }
 
@@ -22,8 +34,24 @@ function playerSelection() {
 function playRound() {
     computerSelection();
     playerSelection();
+
     console.log('Player has chosen ' + playerChoice);
     console.log('Computer has chosen ' + computerChoice);
-}
 
-playRound();
+    if (playerChoice === computerChoice) {
+        console.log("Y'all chose the same move! It's a tie!")
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        playerWin();
+    }
+    else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        playerWin();
+    }
+    else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        playerWin();
+    }
+    else {
+        computerWin();
+    }
+    console.log('The current score is... Player: ' + playerScore + ' and Computer: ' + computerScore);
+}
