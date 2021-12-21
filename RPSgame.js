77ac6dc176b575d6d22260f015db85c3f3de2493
Chandler <chandler_morgan@hotmail.com>
2021-12-21 02:53:12 -0800

    First semi-playable version, no scores

--- RPSgame.js
+++ RPSgame.js
@@ -1,1 +1,29 @@
-console.log("testing!!")
+// Just Defining variables to be changed later
+let computerChoice = 'Computer is failing';
+let playerChoice = 'Player is not working';
+
+function computerSelection() {
+    computerChoice = Math.floor(Math.random() * 3);
+        if (computerChoice === 0) {
+            computerChoice = 'Rock';
+        }
+        else if (computerChoice === 1) {
+            computerChoice = 'Paper';
+        }
+        else {
+            computerChoice = 'Scissors';
+        }
+}
+
+function playerSelection() {
+    playerChoice = prompt("What's youre move?");
+}
+
+function playRound() {
+    computerSelection();
+    playerSelection();
+    console.log('Player has chosen ' + playerChoice);
+    console.log('Computer has chosen ' + computerChoice);
+}
+
+playRound();
