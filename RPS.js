8064c72e1708bea75140b967d4ab1d13dfdc2d4d
Chandler <chandler_morgan@hotmail.com>
2021-12-31 02:59:12 -0800

    Before working weather models 1:30am

--- RPS.js
+++ RPS.js
@@ -1,3 +1,20 @@
+// Defining Variables to be used at a later time
+let computerSelection = null;
+let playerSelection = null;
+
 function computerPlay() {
+    computerSelection = Math.floor(Math.random() * 3 + 1); // Makes computer selection choose randomly 1-3
+    // Makes changes the computers choice to readable moves (Rock, Paper, Scissors)
+    if (computerSelection === 1) {
+        computerSelection = 'Rock';
+    }
+    else if (computerSelection === 2) {
+        computerSelection = 'Paper';
+    }
+    else {
+        computerSelection = 'Scissors';
+    }
+}
+function playRound() {
     
 }
